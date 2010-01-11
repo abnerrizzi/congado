17311d96e8b27df7b468b3fcdda414eae1ec3099
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-01-11 12:51:47 +0000

    Remocao do jquery das views e implementado os includes direto no layout
    git-svn-id: file:///app/congado/trunk@14 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

---
+++ public/scripts/library/fichario_search.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+//	window.alert(baseUrl);
+	$("#add").parent().before('<td width="1"><a href="#" title="Pesquisar"><img src="' + baseUrl + '/images/search.png" alt="Pesquisar"/></a></td>');
+});
