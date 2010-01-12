d3ccde631e9969c1a43e54f4479ec93906ae1b61
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-01-12 18:21:29 +0000

    ao aobrir o fichario se o animal possuir observacoes, eh mostrada uma tela de dialogo com tal informacao.
    git-svn-id: file:///app/congado/trunk@20 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- public/scripts/library/fichario_search.js
+++ public/scripts/library/fichario_search.js
@@ -1,25 +1,6 @@
 $(document).ready(function() {
-	$("#add")
-			.parent()
-			.before(
-					'<td width="2%" align="right"><a href="#" id="search" title="Pesquisar"><img src="' + baseUrl + '/images/search.png" alt="Pesquisar"/></a></td>');
-
-	$('#dialog').dialog(
-			{
-				modal: true,
-				draggable: true,
-				closeOnEscape: true,
-				width: 600,
-				height: 300,
-//				buttons : {
-//					"Ok" : function() {
-//						$(this).dialog("close");
-//					}
-//				}
-			});
 
 	$("#search").click(function(){$("#dialog").dialog('open');});
-
 
 });
 
