e4750a97a83127468d9347ed03750676b67e7ad7
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-01-21 17:06:51 +0000

    git-svn-id: file:///app/congado/trunk@38 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -21,7 +21,7 @@ class ErrorController extends Zend_Controller_Action
 		
 				// 404 error -- controller or action not found
 				$this->getResponse()->setHttpResponseCode(404);
-				$this->view->message = 'Page not found';
+				$this->view->message = 'Página não encontrada';
 				break;
 			default:
 				// application error 
