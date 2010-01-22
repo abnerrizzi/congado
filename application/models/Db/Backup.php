5c225da3edc1ce88979657dcbecd0cc3bc6ab892
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-01-22 12:36:40 +0000

    git-svn-id: file:///app/congado/trunk@41 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- application/models/Db/Backup.php
+++ application/models/Db/Backup.php
@@ -11,13 +11,59 @@
 class Model_Db_Backup extends Model_Db
 {
 
+	private $config;
+
+	public function init()
+	{
+		$this->config = Zend_Registry::get('configuration')->resources->db;
+		$backupDir = $this->config->backup->path;
+
+		if (!is_dir($backupDir)) {
+			if (!@mkdir($backupDir)) {
+				$msg = "Erro criando/acessando diretório de backup\n";
+				$msg .= $backupDir;
+				throw new Zend_Exception($msg);
+			}
+		}
+
+		$backupDir = realpath($backupDir);
+		print '<pre>';
+		if ($this->config->backup->enable == 1) {
+			print_r($this->config->params);
+			$backupFile = '"'.$backupDir . $this->config->params->dbname
+						. date("-Ymd_His") . '.sql"';
+			$backupCommand = "mysqldump"
+				. " -h ".$this->config->params->host
+				. " --user=".$this->config->params->username
+				. " --password=".$this->config->params->password
+				. " ".$this->config->params->dbname
+				. " > ".$backupFile
+			;
+
+			// Execute backup command
+			system($backupCommand);
+			$backupFile = str_replace('\\', '/', $backupFile);
+			print $backupFile;
+			var_dump(is_file($backupFile));
+			die();
+		} else {
+			die('nao vai fazer');
+		}
+		print $backupDir;
+		die();
+		
+	}
 	public function export()
 	{
 
 		$db = Zend_Registry::get('database');
 		$db = $db->getConfig();
+		$config = Zend_Registry::get('resources');
+		print '<pre>';
+		print_r($config);
 
-		$__fileName = $db['dbname'] ."-". date('YmdHi') . ".sql";
+		$__fileName  = dirname(dirname(__FILE__)); 
+		$__fileName .= $db['dbname'] ."-". date('YmdHi') . ".sql";
 
 		$__commandLine = "mysqldump " . $db['dbname'] . " --user=" . $db['username'] . " --password=" . $db['password'];
 		$__commandLine .= " > " . $__fileName;
