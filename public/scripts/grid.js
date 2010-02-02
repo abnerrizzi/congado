ae13f9296ffbb2583bd2db611d491225a90e25b8
abnerrizzi <abner.rizzi@gmail.com>
2010-02-02 13:19:35 +0000

    ajuste no grid ao clicar para mudar para a classe correta
    git-svn-id: file:///app/congado/trunk@61 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- public/scripts/grid.js
+++ public/scripts/grid.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
+
 	$("#null table tr[class*='row'] td").mouseover(function(){
-		$(this).parent().toggleClass('rowon');
-	}).mouseout(function(){
-		$(this).parent().toggleClass('rowon');
-	}).dblclick(function(){
+        if ($(this).parent().hasClass('selected') == false) {
+            $(this).parent().addClass('rowon');
+        }
+	});
+
+	$("#null table tr[class*='row'] td").mouseout(function(){
+        if ($(this).parent().hasClass('selected') == false) {
+            $(this).parent().removeClass('rowon');
+        }
+	});
+
+	$("#null table tr[class*='row'] td").dblclick(function(){
 		id = $(this).parent().attr('id').substr(2);
 		params = new Array();
 		params['id'] = id;
 		postToURL(baseUrl + editUrl + '/id/'+id, '', 'get');
-	}).click(function(){
+	});
+
+	$("#null table tr[class*='row'] td").click(function(){
 		/*
 		 * Seleciona e deseleciona as linhas quando sao clicadas
 		 * caso a linha ja tenha sido selecionada, ao clicar novamente
 		 * a mesma perdera a selecao
 		 */
 		rows = $(this).parent().parent().find("tr[class*='selected']");
+		$(this).parent().toggleClass('rowon');
 		$(this).parent().addClass('selected');
 		rows.each(function(i){
 			$(rows[i]).removeClass('selected');
