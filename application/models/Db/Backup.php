6b0c5ea21b643e39a89606450b81567d77be7a04
abnerrizzi <abner.rizzi@gmail.com>
2010-03-18 13:39:30 +0000

    Ajuste no backup, cria o diretorio caso o mesmo nao exista.
    git-svn-id: file:///app/congado/trunk@118 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- application/models/Db/Backup.php
+++ application/models/Db/Backup.php
@@ -42,7 +42,7 @@ final class Model_Db_Backup extends Model_Db
 	{
 
 		if (!is_dir($this->backupDir)) {
-			if (!@mkdir($this->backupDir)) {
+			if (!@mkdir($this->backupDir, null, true)) {
 				$msg = "Erro criando/acessando diretório de backup: (".$this->backupDir.")";
 				throw new Zend_Exception($msg);
 			}
