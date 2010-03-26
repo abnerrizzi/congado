bbf968e1147b4e06a5eb35b7e23b436fe93e87da
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-03-26 13:41:16 +0000

    git-svn-id: file:///app/congado/branches/abner-dev@171 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- branches/abner-dev/public/scripts/library/fichario_index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-
-	$("#add").parent().before(
-		'<td width="2%" align="right"><a href="javascript:void(0);" id="search" title="Pesquisar"><img src="' + baseUrl + '/images/search.png" alt="Pesquisar"/></a></td>'
-	);
-
-});
