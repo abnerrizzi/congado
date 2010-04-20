a883b85dded286281ee23baf6e13d52f3fb1ad28
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-04-20 13:51:30 +0000

    sql para importacao de cobertura.
    git-svn-id: file:///app/congado/trunk@240 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- sql.php
+++ sql.php
@@ -1,30 +1,93 @@
 <pre>
 <?
+
 /*
 ip 192.163.2.44
 gw 192.163.1.11
 
 proxy 192.163.2.8:1082
  */
-$link = mysql_connect('localhost', 'root', '') or die(mysql_error());
-$file = 'scripts/moviment.csv';
+$link = mysql_connect('127.0.0.1', 'root', '') or die(mysql_error());
+$file = 'scripts/cobertur.csv';
 $handle = fopen ($file, 'r');
 $z=0;
 
+$inline = true;
+
+if ($inline) {
+	print "insert into `congado-dev`.`cobertura` values ";
+}
 while (!feof($handle))
 {
 
-	$atual = fgetcsv($handle, 10240, '|', "'");
+	$atual = fgetcsv($handle, 10240, ';', "'");
 
-	print "insert into `import`.`moviment` values (NULL, ";
-
+	if ($inline) {
+		print " (NULL, ";
+	} else {
+		print "insert into `congado-dev`.`cobertura` values (NULL, ";
+	}
 	$cols = count($atual);
 	for ($i=0; $i < $cols; $i++)
 	{
 		// dt_nascimento
-		if ($i == 3) {
-			print "'" . converte_data($atual[$i]) . "', ";
+		if ($i == 2 || $i == 4) {
+			$dt = converte_data($atual[$i]);
+			if ($i == 2) {
+				$dt2 = $dt;
+			} elseif ($i == 4) {
+				$dt4 = $dt;
+			}
 			continue;
+		} elseif ($i == 3) {
+			if ($dt2 != NULL) {
+				if ($atual[$i] != "") {
+					$dt2 .= " " . substr($atual[$i], 0, strlen($atual[$i])-2) .":". substr($atual[$i], -2);
+					print "'" . $dt2 . "', ";
+					unset($dt2);
+					continue;
+				}
+			} else {
+				print "NULL, ";
+				unset($dt2);
+				continue;
+			}
+		} elseif ($i == 5) {
+			if ($dt4 != NULL) {
+				if ($atual[$i] != "") {
+					$dt4 .= " " . substr($atual[$i], 0, (strlen($atual[$i])-2)) .":". substr($atual[$i], -2);
+					print "'" . $dt4 . "', ";
+					unset($dt4);
+					continue;
+				}
+			} else {
+				print "NULL, ";
+				unset($dt4);
+				continue;
+			}
+		} elseif ($i == 11) {
+			if ($atual[$i] != "") {
+				$query = "SELECT id FROM `congado-dev`.inseminador WHERE cod = '".$atual[$i]."'";
+				$result = mysql_query($query);
+				$row = mysql_fetch_row($result);
+				print $row[0] . ', ';
+				unset($row);
+				continue;
+			}
+		} elseif ($i == 17) {
+			if ($atual[$i] != "") {
+				$query = "SELECT id FROM `congado-dev`.lote WHERE cod = '".$atual[$i]."' and fazenda_id = '".$atual[0]."'";
+				$result = mysql_query($query);
+				$row = mysql_fetch_row($result);
+//				var_dump($row);
+				if (!$row[0]) {
+					print "NULL";
+				} else {
+					print $row[0];
+				}
+				unset($row);
+				continue;
+			}
 		}
 		/*
 		// criador_id
@@ -129,9 +192,13 @@ while (!feof($handle))
 			}
 		}
 	}
-	print ");\n";
+	if ($inline) {
+		print "),\n";
+	} else {
+		print ");\n";
+	}
 	$z++;
-//	if ($z == 650) {
+//	if ($z == 5521) {
 //		die();
 //	}
 	continue;
@@ -144,7 +211,11 @@ fclose($handle);
 
 
 function converte_data($date) {
-	return substr($date, 6) ."-". substr($date, 3, 2) ."-". substr($date, 0, 2);
+	if ($date != "") {
+		return substr($date, 6) ."-". substr($date, 3, 2) ."-". substr($date, 0, 2);
+	} else {
+		return NULL;
+	}
 }
 
 ?>
