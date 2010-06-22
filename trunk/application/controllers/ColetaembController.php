1f019263f5bdbead7d2cf4d1dfb8901fc3284915
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-06-22 14:56:23 +0000

    git-svn-id: file:///app/congado/branches/abner-dev@351 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- trunk/application/controllers/ColetaembController.php
+++ trunk/application/controllers/ColetaembController.php
@@ -5,12 +5,62 @@ class ColetaembController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $auth = Zend_Auth::getInstance();
+		$this->view->auth = $auth->hasIdentity();
+		$this->view->title = 'Coleta de Embriões';
+		$this->view->baseUrl = $this->getRequest()->getBaseUrl();
     }
 
     public function indexAction()
     {
-        // action body
+        $gridModel = new Model_Grid($this->view->title);
+        $coletaModel = new Model_Db_ColetaEmbriao();
+
+        $_page	= $this->_getParam('page', 1);
+		$_by	= $this->_getParam('by', 'id');
+		$_order	= $this->_getParam('sort', 'asc');
+
+		$result = $coletaModel->getPaginatorAdapter($_by, $_order, array('id', 'cod', 'dsc'));
+
+		/*
+		 * Paginator
+		 */
+		$paginator = Zend_Paginator::factory($result);
+		if (intval(Zend_Auth::getInstance()->getIdentity()->perpage) > 0) {
+			$paginator->setItemCountPerPage(intval(Zend_Auth::getInstance()->getIdentity()->perpage));
+		} else {
+			$paginator->setItemCountPerPage(Zend_Registry::get('configuration')->pagination->itemCoutPerPage);
+		}
+		$paginator->setCurrentPageNumber($_page);
+
+		/*
+		 * Fields
+		 */
+		$fields[] = new Model_Grid_Fields('dsc', 'Descri&ccedil;&atilde;o', 150);
+		$fields[] = new Model_Grid_Fields('sexo', 'Sexo', 70);
+
+		/*
+		 * Grid Model
+		 */
+		$gridModel->setBaseUrl($this->view->baseUrl);
+		$gridModel->setSorting(true);
+		$gridModel->setPaginator($paginator);
+		$gridModel->setFields($fields);
+		$gridModel->setEdit(array(
+			'module'	=> 'categoria',
+			'action'	=> 'edit',
+		));
+		$gridModel->setDelete(array(
+			'module'	=> 'categoria',
+			'action'	=> 'delete',
+		));
+		$gridModel->setAdd(array(
+			'module'	=> 'categoria',
+			'action'	=> 'add',
+		));
+
+		$this->view->sort = $_order;
+		$this->view->grid = $gridModel;
     }
 
 
