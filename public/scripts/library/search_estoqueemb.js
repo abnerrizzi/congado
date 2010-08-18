8741f7ebc63239b9b2ff7138f40a9f3c833c4849
abnerrizzi <abner.rizzi@gmail.com>
2010-08-18 14:16:45 +0000

    asd
    git-svn-id: file:///app/congado/trunk@389 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- public/scripts/library/search_estoqueemb.js
+++ public/scripts/library/search_estoqueemb.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	$("#add>a").before(
 			'<a class="UIButton UIButton_Gray UIActionButton" href="javascript:void(0);" id="search" title="Pesquisar">\n'+
 			'<span class="UIButton_Text">\n'+
-			'<span class="UIButton_Search UIButton_IconNoSpriteMap_Search UIButton_IconSmallMonochromatic" style="background: url(/congado/public/images/search.png) no-repeat;background-position: 4pt 1px;"></span>\n'+
+			'<span class="UIButton_Search UIButton_IconNoSpriteMap_Search UIButton_IconSmallMonochromatic" style="background: url(/images/search.png) no-repeat;background-position: 4pt 1px;"></span>\n'+
 			'Pesquisar\n'+
 			'</span></a>\n'+
 			'\n'
