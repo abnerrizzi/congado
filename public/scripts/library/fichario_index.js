fd5c332720ad3d1f20fbd55b134566eaf7289b69
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-09-13 19:59:20 +0000

    Atualizacao do icone no botao de pesquisar
    git-svn-id: file:///app/congado/trunk@431 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- public/scripts/library/fichario_index.js
+++ public/scripts/library/fichario_index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 //	$(
 		'<a class="UIButton UIButton_Gray UIActionButton" href="javascript:void(0);" id="search" title="Pesquisar">\n'+
 		'<span class="UIButton_Text">\n'+
-		'<span class="UIButton_Search UIButton_IconNoSpriteMap_Search UIButton_IconSmallMonochromatic" style="background: url(/images/search.png) no-repeat;background-position: 4pt 1px;"></span>\n'+
+		'<span class="UIButton_Search UIButton_IconNoSpriteMap_Search UIButton_IconSmallMonochromatic"></span>\n'+
 		'Pesquisar\n'+
 		'</span></a>\n'
 	);
