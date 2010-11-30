7cd88083cd9e0622e34993484a987f847d410a46
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2010-11-30 19:43:57 +0000

    git-svn-id: file:///app/congado/trunk@553 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- public/scripts/library/embriao/estoque.js
+++ public/scripts/library/embriao/estoque.js
@@ -1,42 +1,28 @@
 $(document).ready(function() {
 
-	$('#doadora').after(
-		'<a href="javascript:void(0);" onclick="return showfilter_animal(\''+baseUrl+'/json/animal/sexo/f\', \'doadora\')">\n'+
-		'<img alt="" src="'+baseUrl+'/images/search.png"/>\n'+
-		'</a>\n'
+	$("#add>a").before(
+			'<a class="UIButton UIButton_Gray UIActionButton" href="javascript:void(0);" id="search" title="Pesquisar">\n'+
+			'<span class="UIButton_Text">\n'+
+			'<span class="UIButton_Search UIButton_IconNoSpriteMap_Search UIButton_IconSmallMonochromatic""></span>\n'+
+			'Pesquisar\n'+
+			'</span></a>\n'+
+			'\n'
 	);
+	$('#add a#search').click(function(){search.coletaEmbriao();});
 
-	$('#touro').after(
-			'<a href="javascript:void(0);" onclick="return showfilter_animal(\''+baseUrl+'/json/animal/sexo/m\', \'touro\')">\n'+
-			'<img alt="" src="'+baseUrl+'/images/search.png"/>\n'+
-			'</a>\n'
-	);
+	if (checkAddUrl(this.location.href) || checkEditUrl(this.location.href)) {
 
-	$('#criador').after(
-			'<a href="javascript:void(0);" onclick="return showfilter_criador(\''+baseUrl+'/json/criador\', \'criador\')">\n'+
-			'<img alt="" src="'+baseUrl+'/images/search.png"/>\n'+
-			'</a>\n'
-	);
+		makeDateField('#dt_coleta', false, new Date());
 
-	$("#doadora_cod").change(function(){
-		changeSelectAnimalBySexo(this, 'F');
-	});
-	$("#touro_cod").change(function(){
-		changeSelectAnimalBySexo(this, 'M');
-	});
+		$("#doadora_cod").change(function(){change.animal(this, 'f');});
+		$("#touro_cod").change(function(){change.animal(this, 'm');});
+		$("#criador_cod").change(function(){change.select(this);});
 
-	$("#criador_cod").change(changeSelect);
+		addSearchIcon('doadora', baseUrl+'/json/fichario/sexo/f', 'filter.animal', 600, 240);
+		addSearchIcon('touro', baseUrl+'/json/fichario/sexo/m', 'filter.animal', 600, 240);
+		addSearchIcon('criador', baseUrl+'/json/criador', 'filter.criador', 600, 240);
 
-	makeDateField('#dt_coleta', false, new Date());
-
-	// workaround to adjust size in mozilla
-	$.each($.browser, function(i) {
-		if ($.browser.mozilla) {
-			var _Height = parseInt(20);
-		} else {
-			var _Height = parseInt(0);
-		}
-	});
+	}
 
 });
 
