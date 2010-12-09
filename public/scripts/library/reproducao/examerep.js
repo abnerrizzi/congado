a0b1293c9aa4d8b150aba2bbd0acc1cad3122358
abnerrizzi <abner.rizzi@gmail.com>
2010-12-09 17:48:29 +0000

    git-svn-id: file:///app/congado/trunk@572 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- public/scripts/library/reproducao/examerep.js
+++ public/scripts/library/reproducao/examerep.js
@@ -14,4 +14,13 @@ $(document).ready(function() {
 		makeDateField('#data');
 	}
 
+	$("#add>a").before(
+			'<a class="UIButton UIButton_Gray UIActionButton" href="javascript:void(0);" id="search" title="Pesquisar">\n'+
+			'<span class="UIButton_Text">\n'+
+			'<span class="UIButton_Search UIButton_IconNoSpriteMap_Search UIButton_IconSmallMonochromatic""></span>\n'+
+			'Pesquisar\n'+
+			'</span></a>\n'+
+			'\n'
+	);
+	$('#add a#search').click(function(){search.exameRep();});
 });
