e2dfa68d5bca60e5e8c8d4cafe40d4e2503c954c
bacteria_ <bacteria_@2e285716-b68e-6c46-99ea-6b3d53fc1b9f>
2011-01-25 16:45:51 +0000

    git-svn-id: file:///app/congado/trunk@604 2e285716-b68e-6c46-99ea-6b3d53fc1b9f

--- application/modules/test/controllers/IndexController.php
+++ application/modules/test/controllers/IndexController.php
@@ -80,6 +80,18 @@ class Test_IndexController extends Zend_Controller_Action
 		$this->view->selected = Zend_Auth::getInstance()->getIdentity()->fazenda_id;
 	}
 
+	public function mymenuAction()
+	{
+		$fazendaModel = new Model_Db_Fazenda();
+		$fazendas = $fazendaModel->getFazendaByUser(Zend_Auth::getInstance()->getIdentity()->id);
+		foreach ($fazendas as $key) {
+			$x[$key['id']] = $key['descricao'];
+		}
+
+		$this->view->fazendas = $x;
+		$this->view->selected = Zend_Auth::getInstance()->getIdentity()->fazenda_id;
+	}
+
 	public function menufcbkAction()
 	{
 		$x = array(
